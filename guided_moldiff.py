216281b2dc5effca5a41e7d4a3e1f57fd521f7df
Rokas Elijošius <re344@cam.ac.uk>
2024-12-17 22:48:39 +0200

    scan bond predictor as well

--- guided_moldiff.py
+++ guided_moldiff.py
@@ -24,15 +24,18 @@ from utils.transforms import FeaturizeMol, make_data_placeholder
 
 app = typer.Typer()
 
+
 @dataclass
 class Config:
+    experiment_name: str
     guidance_strength: float
     default_sigma: float
-    element_sigmas: dict[int, float] | None
     num_mols: int
     batch_size: int
-    experiment_name: str
-    max_size: int= 20
+    element_sigmas: dict[int, float] | None = None
+    bond_guidance_strength: float = 0
+    max_size: int = 20
+
 
 def load_molecule_from_smiles(smiles):
     mol = Chem.MolFromSmiles(smiles)
@@ -42,13 +45,14 @@ def load_molecule_from_smiles(smiles):
     mol = Chem.RemoveHs(mol)
     return mol
 
+
 def mol_to_ase_atoms(mol):
     symbols = [atom.GetSymbol() for atom in mol.GetAtoms()]
     positions = mol.GetConformer().GetPositions()
     return ase.Atoms(symbols, positions)
 
 
-def traj_to_ase(out, featurizer, idx:int|None=None):
+def traj_to_ase(out, featurizer, idx: int | None = None):
     """Convert trajectory to ASE Atoms list."""
     traj = []
     nodes_to_elements = featurizer.nodetype_to_ele.copy()
@@ -66,9 +70,10 @@ def traj_to_ase(out, featurizer, idx:int|None=None):
         traj.append(atoms)
     return traj
 
+
 @app.command()
 def main(config_path: str):
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         cfg = yaml.safe_load(f)
     config = Config(**cfg)
 
@@ -99,15 +104,19 @@ def main(config_path: str):
     ).to("cuda")
     model.load_state_dict(ckpt["model"])
     model.eval()
-    
-    # ckpt_bond = torch.load("./ckpt/bondpred.pt", map_location="cuda")
-    # bond_predictor = BondPredictor(
-    #     ckpt_bond['config']['model'],
-    #     featurizer.num_node_types,
-    #     featurizer.num_edge_types-1
-    # ).to("cuda")
-    # bond_predictor.load_state_dict(ckpt_bond['model'])
-    # bond_predictor.eval()
+
+    if config.bond_guidance_strength > 0:
+        logging.info("Loading bond predictor.")
+        ckpt_bond = torch.load("./ckpt/bondpred.pt", map_location="cuda")
+        bond_predictor = BondPredictor(
+            ckpt_bond["config"]["model"],
+            featurizer.num_node_types,
+            featurizer.num_edge_types - 1,
+        ).to("cuda")
+        bond_predictor.load_state_dict(ckpt_bond["model"])
+        bond_predictor.eval()
+    else:
+        bond_predictor = None
 
     calc = mace_off("medium", device="cuda", default_dtype="float32")
     z_table = calc.z_table
@@ -117,7 +126,9 @@ def main(config_path: str):
     ase_mol.calc = None
     ase_mol = ase_mol[ase_mol.numbers != 1][core_ids]
 
-    element_sigma_array = np.ones_like(z_table.zs, dtype=np.float32) * config.default_sigma
+    element_sigma_array = (
+        np.ones_like(z_table.zs, dtype=np.float32) * config.default_sigma
+    )
     if config.element_sigmas is not None:
         for element, sigma in config.element_sigmas.items():
             element_sigma_array[z_table.z_to_index(element)] = sigma
@@ -131,24 +142,30 @@ def main(config_path: str):
         element_sigma_array=element_sigma_array,
     )
 
-    pool = {'failed': [], 'finished': [], 'last_frames': []}
-    while len(pool['finished']) < config.num_mols:
-        if len(pool['failed']) > 256:
-            logging.info(f'Experiment NO. {config.experiment_name}: Too many failed molecules. Stop sampling.')
+    pool = {"failed": [], "finished": [], "last_frames": []}
+    while len(pool["finished"]) < config.num_mols:
+        if len(pool["failed"]) > 256:
+            logging.info(
+                f"Experiment NO. {config.experiment_name}: Too many failed molecules. Stop sampling."
+            )
             break
         batch_holder = make_data_placeholder(
-            n_graphs=config.batch_size, device="cuda", max_size=config.max_size)
+            n_graphs=config.batch_size, device="cuda", max_size=config.max_size
+        )
         outputs = model.sample(
             n_graphs=config.batch_size,
             batch_node=batch_holder["batch_node"],
             halfedge_index=batch_holder["halfedge_index"],
             batch_halfedge=batch_holder["batch_halfedge"],
-            bond_predictor=None,
+            bond_predictor=bond_predictor,
+            bond_gui_scale=config.bond_guidance_strength,
             featurizer=featurizer,
             simgen_calc=sim_calc,
             simgen_gui_scale=config.guidance_strength,
         )
-        outputs = {key: [v.cpu().numpy() for v in value] for key, value in outputs.items()}
+        outputs = {
+            key: [v.cpu().numpy() for v in value] for key, value in outputs.items()
+        }
         batch_node = batch_holder["batch_node"].cpu().numpy()
         halfedge_index = batch_holder["halfedge_index"].cpu().numpy()
         batch_halfedge = batch_holder["batch_halfedge"].cpu().numpy()
@@ -158,32 +175,34 @@ def main(config_path: str):
 
         for output_mol in output_list:
             mol_info = featurizer.decode_output(
-                pred_node=output_mol['pred'][0],
-                pred_pos=output_mol['pred'][1],
-                pred_halfedge=output_mol['pred'][2],
-                halfedge_index=output_mol['halfedge_index'],
+                pred_node=output_mol["pred"][0],
+                pred_pos=output_mol["pred"][1],
+                pred_halfedge=output_mol["pred"][2],
+                halfedge_index=output_mol["halfedge_index"],
             )
             try:
                 rdmol = reconstruct_from_generated_with_edges(mol_info)
             except MolReconsError:
-                pool['failed'].append(mol_info)
-                logging.warning('Reconstruction error encountered.')
+                pool["failed"].append(mol_info)
+                logging.warning("Reconstruction error encountered.")
                 continue
-            mol_info['rdmol'] = rdmol
+            mol_info["rdmol"] = rdmol
             smiles = Chem.MolToSmiles(rdmol)
-            mol_info['smiles'] = smiles
-            pool['finished'].append(mol_info)
+            mol_info["smiles"] = smiles
+            pool["finished"].append(mol_info)
             last_frame = traj_to_ase(output_mol["traj"], featurizer, -1)
-            pool['last_frames'].append(last_frame)
-            aio.write(results_path/"last_frames.xyz", last_frame, append=True)
+            pool["last_frames"].append(last_frame)
+            aio.write(results_path / "last_frames.xyz", last_frame, append=True)
 
-            sdf_dir = results_path / 'SDF'
+            sdf_dir = results_path / "SDF"
             sdf_dir.mkdir(exist_ok=True)
-            with open(results_path / 'SMILES.txt', 'a') as smiles_f:
-                current_index = len(pool['finished']) - 1
-                smiles_f.write(mol_info['smiles'] + '\n')
-                rdmol = mol_info['rdmol']
-                Chem.MolToMolFile(rdmol, str(sdf_dir / f'{current_index}.sdf'))
+            with open(results_path / "SMILES.txt", "a") as smiles_f:
+                current_index = len(pool["finished"]) - 1
+                smiles_f.write(mol_info["smiles"] + "\n")
+                rdmol = mol_info["rdmol"]
+                Chem.MolToMolFile(rdmol, str(sdf_dir / f"{current_index}.sdf"))
+
 
 if __name__ == "__main__":
-    app()
+    # app()
+    main("./test_config.yaml")
--- random_screen.py
+++ random_screen.py
@@ -8,14 +8,25 @@ num_configs = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 output_dir = "screen_configs"
 os.makedirs(output_dir, exist_ok=True)
 
+low = 1e-3
+high = 2e-1
+
 for i in range(num_configs):
+    # Sample guidance_strength in log scale
+    guidance_strength = low * (high/low)**np.random.uniform(0, 1)
+    
+    # Sample bond_guidance_strength
+    bond_guidance_strength = 0 if np.random.rand() < 0.5 else 1e-4
+
     config = {
-        "guidance_strength": float(np.random.uniform(0.0, 1.0)),
+        "guidance_strength": float(guidance_strength),
         "default_sigma": float(np.random.uniform(0.05, 0.8)),
         "element_sigmas": {7: float(np.random.uniform(0.1, 0.5))},
+        "bond_guidance_strength": bond_guidance_strength,
         "num_mols": 1280,
         "batch_size": 128,
         "experiment_name": f"exp_{i}"
     }
+
     with open(os.path.join(output_dir, f"config_{i}.yaml"), "w") as f:
         yaml.safe_dump(config, f)
